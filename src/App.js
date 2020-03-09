8cacaf1be0a47b9ca786b21de21f2dff4536636c
Michael <msiller455@gmail.com>
2020-03-09 11:02:12 -0700

    enables hover over effect for h1's

--- src/App.js
+++ src/App.js
@@ -7,17 +7,16 @@ import SnowOcean from './images/snow-ocean.jpg';
 
 const App = () => {
   const [togglePic, setTogglePic] = useState(false)
+  const [currentPic, setCurrentPic] = useState("")
 
   const el = (e) => {
-    if(e.target.id === "new-fast") {
-      setTogglePic(true)
+    setTogglePic(true)
+    setCurrentPic(e.target.id)
   }
-}
 
   const clearEl = (e) => {
-    if(e.target.id === "new-fast") {
-      setTogglePic(false)
-    }
+    setTogglePic(false)
+    setCurrentPic("")
   }
 
   return (
@@ -29,9 +28,9 @@ const App = () => {
           </div>
           <h1 className="headline-name">Brent Ahrens</h1>
           <p className="headline-desc">Brent Ahrens is easily the best rapper in the entire world. Memeber of the legendary Hip Hop duo Young Planet his legacy has already been cemented in the world of music.</p>
-          <img className={togglePic === true ? "show" : "hide"} src={NewFast} alt="new fast automatic" />
-          <img className={togglePic === true ? "show" : "hide"} src={Orare} alt="orare" />
-          <img className={togglePic === true ? "show" : "hide"} src={SnowOcean} alt="snow-ocean" />
+          <img className={(togglePic && currentPic==="new-fast") ? "show" : "hide"} src={NewFast} alt="new fast automatic" id="new-fast" />
+          <img className={(togglePic && currentPic==="orare") ? "show" : "hide"} src={Orare} alt="orare" id="orare"/>
+          <img className={(togglePic && currentPic==="snow-ocean") ? "show" : "hide"} src={SnowOcean} alt="snow-ocean" id="snow-ocean" />
           <div className="main-bottom">
             <h1 className="headline-name">Rapper</h1>
             <h1 className="headline-name">Writer</h1>
@@ -44,8 +43,8 @@ const App = () => {
             <p className="main-btns--group">Follow</p>
           </div>
           <h1 className="headline-name" id="new-fast" onMouseOver={el} onMouseLeave={clearEl}>New Fast Automatic</h1>
-          <h1 className="headline-name" id="snow-ocean">Snow Ocean</h1>
-          <h1 className="headline-name" id="orare">Orare</h1>
+          <h1 className="headline-name" id="snow-ocean" onMouseOver={el} onMouseLeave={clearEl}>Snow Ocean</h1>
+          <h1 className="headline-name" id="orare" onMouseOver={el} onMouseLeave={clearEl}>Orare</h1>
         </div>
       </div>
     );
